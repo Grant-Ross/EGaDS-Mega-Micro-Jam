963fae064abd50dc075504a94c8e9b87a7dab66f
Grant-Ross <ghross@utexas.edu>
2020-12-23 00:25:30 -0600

    bug fix 2

--- Assets/Base Files (Dont Touch)/MainGameManager.cs
+++ Assets/Base Files (Dont Touch)/MainGameManager.cs
@@ -159,10 +159,9 @@ public class MainGameManager : MonoBehaviour
     private GameInfo GetNextGame()
     {
         GameInfo game = new GameInfo();
-        game.id = Random.Range(0, _remainingGames.Count -1);
+        game.id = 0;
         game.name = _remainingGames[game.id];
         game.id += indexOffset;
-        //remainingGames.Remove(game);
         return game;
     }
     
